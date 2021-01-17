fd4c715be1f776bb229bafc71d15a85369d6af21
Safnas <iamsafnas@gmail.com>
2021-01-17 17:39:43 +0530

    created console email Service app and added rabbit mq connection

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using RabbitMQ.Client;
+
+
+namespace ExploreCalifornia.EmailService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            // declare resources here, handle consumed events, etc
+
+            channel.Close();
+            connection.Close();
+
+        }
+    }
+}
